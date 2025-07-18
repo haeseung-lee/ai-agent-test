0ec9af8f0aaac788bcaf5c277cb3b1c634085890
haeseung-lee <dlgotmd93@gmail.com>
2025-07-18 17:48:37 +0900

    feat. Tool 기능 활용 시도

--- src/main/java/com/example/ai_agent_test/gemini/controller/ChatController.java
+++ src/main/java/com/example/ai_agent_test/gemini/controller/ChatController.java
@@ -3,6 +3,9 @@ package com.example.ai_agent_test.gemini.controller;
 
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.function.Function;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -14,13 +17,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.ai_agent_test.gemini.tools.DateTimeTools;
+
 @RestController
 public class ChatController {
 
 	private final ChatClient chatClient;
 	
 	public ChatController(ChatClient.Builder chatClientBuilder) {
-		this.chatClient = chatClientBuilder.build();
+		this.chatClient = chatClientBuilder
+						.build();
 	}
 	
 	/**
@@ -32,6 +38,7 @@ public class ChatController {
 	public String chat(@RequestBody String message) {
 		return chatClient.prompt()
 						.user(message)
+						.tools(new DateTimeTools())
 						.call()
 						.content();
 	}
@@ -47,10 +54,11 @@ public class ChatController {
 	public String chatWithBinaryImage(@RequestParam("image") MultipartFile imageFile, 
             @RequestParam("query") String query) throws IOException {
 		// 바이너리 이미지 데이터를 Media 객체로 변환
-		byte[] data = imageFile.getBytes();
+		byte[] data = imageFile.getBytes(); 
 		Resource imageResource = new ByteArrayResource(data);
-        return chatClient.prompt().user(u -> u.text(query)
+		
+		Map<String, Object> options = Map.of("languageCode", "ko-KR");
+        return chatClient.prompt().user(u -> u.text(query + "key값을 인물이면 person, 사물이면 thing, 장소면 place로 응답(그 외 내용설명 포함)")
         							.media(MimeTypeUtils.IMAGE_PNG, imageResource)).call().content();
 	}
-
 }
